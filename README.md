1ed8cc85f27538266beb493914e12224d8b5ded1
subramani03 <subramanimurugan420@gmail.com>
2024-07-26 12:57:23 +0530

    CRA and configured Tailwind css

---
+++ README.md
@@ -0,0 +1,4 @@
+# Netflix GPT
+
+- Create react app
+- configured Tailwind css
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl text-red-700 m-[500px]">
+     hello World
+    </div>
+  );
+}
+
+export default App;
